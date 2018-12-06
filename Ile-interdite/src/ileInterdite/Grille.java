e87fd532b7f072107a49089b6a3e6fb90dd36932
grosa <grosa@pc-dg-027-11.iut2.upmf-grenoble.fr>
2018-12-06 09:53:40 +0100

    Ajout de la fonction de déplacement pour le plongeur Changement de l'indexation des cases avec un hashmap

--- Ile-interdite/src/ileInterdite/Grille.java
+++ Ile-interdite/src/ileInterdite/Grille.java
@@ -6,6 +6,7 @@
 package ileInterdite;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  *
@@ -13,14 +14,14 @@ import java.util.ArrayList;
  */
 public class Grille {
     private static int longueurTerrain = 6;
-    private Tuile[][] tuiles = new Tuile[longueurTerrain][longueurTerrain];
+    private HashMap<Position, Tuile> tuiles = new HashMap();
     
     public Grille() {
         
     }
     
     public Tuile getTuile(Position p) {
-        return tuiles[p.x][p.y];
+        return tuiles.get(new Position(p.x, p.y));
     }
     
     // Retourne un tableau des 4 tuiles adjascentes à la position pos selon ce
@@ -28,32 +29,43 @@ public class Grille {
     //           tuile[0]
     //    tuile[3] pos tuile[1]
     //           tuile[2]
-    public Tuile[] tuilesAdjacentesCroix(Position posTuile) {
-        Tuile[] tuilesAdjacentes = new Tuile[4];
-        tuilesAdjacentes[0] = tuiles[posTuile.x][posTuile.y+1];
-        tuilesAdjacentes[1] = tuiles[posTuile.x+1][posTuile.y];
-        tuilesAdjacentes[2] = tuiles[posTuile.x][posTuile.y-1];
-        tuilesAdjacentes[3] = tuiles[posTuile.x-1][posTuile.y];
+    public ArrayList<Tuile> tuilesAdjacentesCroix(Position posTuile) {
+        ArrayList<Tuile> tuilesAdjacentes = new ArrayList();
+        Position pos[] = {
+            new Position(posTuile.x, posTuile.y+1),
+            new Position(posTuile.x+1, posTuile.y),
+            new Position(posTuile.x, posTuile.y-1),
+            new Position(posTuile.x-1, posTuile.y)
+        };
+        
+        if (tuiles.containsKey(pos[0]))
+            tuilesAdjacentes.add(tuiles.get(pos[0]));
+        if (tuiles.containsKey(pos[1]))
+            tuilesAdjacentes.add(tuiles.get(pos[1]));
+        if (tuiles.containsKey(pos[2]))
+            tuilesAdjacentes.add(tuiles.get(pos[2]));
+        if (tuiles.containsKey(pos[3]))
+            tuilesAdjacentes.add(tuiles.get(pos[3]));
         
         return tuilesAdjacentes;
     }
     
-    // Retourne un tableau des 4 tuiles adjascentes à la position pos selon ce
+    // Retourne un tableau des 4 tuiles adjacentes à la position pos selon ce
     // pattern:
     //    tuile[0] tuile[1] tuile[2]
     //    tuile[3]   pos    tuile[4]
     //    tuile[5] tuile[6] tuile[7]
-    public Tuile[] tuilesAdjacentesCarre(Position posTuile) {
-        Tuile[] tuilesAdjacentes = new Tuile[8];
+    public ArrayList<Tuile> tuilesAdjacentesCarre(Position posTuile) {
+        ArrayList<Tuile> tuilesAdjacentes = new ArrayList();
         
-        int i = 0;
         for (int y = 1; y <= -1; y--) {
             for (int x = -1; x <= 1; x++) {
                 if (x == 0 && y == 0)
                     continue;
                 else {
-                    tuilesAdjacentes[i] = tuiles[posTuile.x+x][posTuile.y+y];
-                    i += 1;
+                    Position pos = new Position(posTuile.x+x, posTuile.y+y);
+                    if (tuiles.containsKey(pos))
+                        tuilesAdjacentes.add(tuiles.get(pos));
                 }
             }
         }
@@ -61,14 +73,13 @@ public class Grille {
         return tuilesAdjacentes;
     }
     
-    //TODO: Remplir
     public ArrayList<Tuile> tuilesSeches() {
         ArrayList<Tuile> tuilesSeches = new ArrayList();
         
-        for (int x = 0; x <= longueurTerrain; x++) {
-            for (int y = 0; y != longueurTerrain; y++) {
-                if (tuiles[x][y].getEtat() == EtatTuile.SECHE) {
-                    tuilesSeches.add(tuiles[x][y]);
+        for (int x = 0; x < longueurTerrain; x++) {
+            for (int y = 0; y < longueurTerrain; y++) {
+                if (tuiles.get(new Position(x, y)).getEtat() == EtatTuile.SECHE) {
+                    tuilesSeches.add(tuiles.get(new Position(x, y)));
                 }
             }
         }
@@ -76,8 +87,31 @@ public class Grille {
         return tuilesSeches;
     }
     
-    public ArrayList<Tuile> tuilesAccessiblesPlongeur(Position posTuile) {
-        ArrayList<Tuile> tuilesSeches = new ArrayList();
-        return tuilesSeches;
+    public ArrayList<Tuile> tuilesAccessiblesPlongeur(Tuile tuile) {
+        ArrayList<Tuile> tuileAccessibles = new ArrayList();
+        ArrayList<Tuile> tuileNonVerifiees = new ArrayList();
+        tuileNonVerifiees.add(tuile);
+        
+        while(!tuileNonVerifiees.isEmpty())
+        {
+            Tuile tuileVerifiee = tuileNonVerifiees.get(0);
+            ArrayList<Tuile> tuilesAdjacentes = new ArrayList();
+            
+            
+            tuilesAdjacentes = this.tuilesAdjacentesCroix(tuileVerifiee.getPosition());
+            
+            for (Tuile tNew : tuilesAdjacentes) {
+                if (tuileAccessibles.contains(tNew)){
+                    tuilesAdjacentes.remove(tNew);
+                }
+            }
+            
+            tuileNonVerifiees.addAll(tuilesAdjacentes);
+            
+            tuileAccessibles.add(tuileVerifiee);
+            tuileNonVerifiees.remove(0);
+        }
+        
+        return tuileAccessibles;
     }
 }
