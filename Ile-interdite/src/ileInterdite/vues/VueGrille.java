0b71434ba2ab2d504766f41a450be78d3b4f864c
VINET Guillaume <vinetg@hostname.iut2.upmf-grenoble.fr>
2018-12-10 11:56:30 +0100

    voila

--- Ile-interdite/src/ileInterdite/vues/VueGrille.java
+++ Ile-interdite/src/ileInterdite/vues/VueGrille.java
@@ -5,12 +5,51 @@
  */
 package ileInterdite.vues;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Observable;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  *
  * @author vinetg
  */
 public class VueGrille extends Observable{
+
+    private final JFrame window ;
+
+
+    @SuppressWarnings("Convert2Lambda")
+    public VueGrille() {
+
+        window = new JFrame();
+        window.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        // Définit la taille de la fenêtre en pixels
+        window.setSize(250, 120);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        window.setLocation(dim.width/2-window.getSize().width/2, dim.height/2-window.getSize().height/2);
+        
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        window.add(mainPanel) ;
+        
+        
+    }
+
     
+    public void afficher() {
+        this.window.setVisible(true);
+    }
+
+    public void close() {
+        this.window.dispose();
+    }
 }
+
+
