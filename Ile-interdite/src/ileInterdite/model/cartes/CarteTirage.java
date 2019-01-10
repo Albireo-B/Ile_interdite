04f33def777bbde4723d7e6fea8eeaccb2579826
GENEVOIS Clement <genevoic@hostname.iut2.upmf-grenoble.fr>
2019-01-10 11:01:58 +0100

    lol

--- Ile-interdite/src/ileInterdite/model/cartes/CarteTirage.java
+++ Ile-interdite/src/ileInterdite/model/cartes/CarteTirage.java
@@ -10,5 +10,8 @@ package ileInterdite.model.cartes;
  * @author vinetg
  */
 public class CarteTirage {
-    
+    //20 cartes trésors (5 pour chacun)
+    //2 cartes montée des eaux 
+    //3 cartes hélicoptère
+    //2 cartes sacs de sable
 }
