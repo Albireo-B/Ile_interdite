74bc5556c9c11fc95b1158f089c36a029370acbd
THEOPHILE Lois <deniaul@hostname.iut2.upmf-grenoble.fr>
2019-01-14 13:52:31 +0100

    début navigateur

--- Ile-interdite/src/ileInterdite/controleur/Controleur.java
+++ Ile-interdite/src/ileInterdite/controleur/Controleur.java
@@ -132,8 +132,16 @@ public class Controleur implements Observer {
      */
     public void gererDeplacement() {
         if (aventurierCourant.getNbAction()>0){
-            if (aventurierCourant.getRole()==Role.Navigateur)
-            proposerTuiles(aventurierCourant.calculDeplacement(grille), Action.DEPLACER,aventurierCourant.getRole());
+            if (aventurierCourant.getRole()==Role.Navigateur){
+                for (Role r : listeRoles){
+                    if (r != Role.Navigateur){
+                        
+                    }
+                }
+            }
+            else{
+                proposerTuiles(aventurierCourant.calculDeplacement(grille), Action.DEPLACER,aventurierCourant.getRole());
+            } 
         }
     }
 
