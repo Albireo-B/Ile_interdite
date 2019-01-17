7b4dc5d0eeb552be1696ea1d9a20dc60336c3d8d
THEOPHILE Lois <deniaul@hostname.iut2.upmf-grenoble.fr>
2019-01-17 08:23:08 +0100

    fin du travail sur la carte helico

---
+++ Ile-interdite/src/ileInterdite/message/MessageGroupePos.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ileInterdite.message;
+
+import utilitaires.Action;
+import ileInterdite.model.Position;
+import java.util.ArrayList;
+import utilitaires.Role;
+
+/**
+ *
+ * @author vinetg
+ */
+public class MessageGroupePos extends MessageGroupe {
+
+    private Position pos;
+
+    /**
+     * On définit le constructeur de MessageGroupePos avec une action Action et une
+     * position Position
+     *
+     * @param action
+     * @param pos
+     */
+    public MessageGroupePos(Action action, Position pos,Role role, ArrayList<Role> roles) {
+        super(action,role, roles);
+        this.pos = pos;
+    }
+
+    //Getters et Setters :
+    /**
+     * @return the pos
+     */
+    public Position getPos() {
+        return pos;
+    }
+
+}
