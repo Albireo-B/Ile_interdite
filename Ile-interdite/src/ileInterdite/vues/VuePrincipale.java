7ebe19a07df11f246d285a614c1b58e957c5fc3a
leptiphoenix <35071358+leptiphoenix@users.noreply.github.com>
2019-01-17 23:29:47 +0100

    Update VuePrincipale.java

--- Ile-interdite/src/ileInterdite/vues/VuePrincipale.java
+++ Ile-interdite/src/ileInterdite/vues/VuePrincipale.java
@@ -85,7 +85,6 @@ public class VuePrincipale extends Observable {
         window.add(panelPrincipal);
         window.setResizable(true);
         
-        
         labelNbPA.setFont(new Font("Arial", Font.BOLD, 20));
         labelNbPA.setForeground(Color.yellow);
         btnTerminerTour.setFont(new Font("Arial", Font.BOLD, 25));
